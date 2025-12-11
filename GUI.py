81472e6ee11ed26c97f3da4ffba241b4bc0b8505
Someonealreadytookmyname <hello.krishsuthar@gmail.com>
2025-12-11 19:06:53 +0530

    "# Upgraded with GUI and working for users"

--- GUI.py
+++ GUI.py
@@ -1,13 +1,39 @@
-import tkinter
+from tkinter import *
+import ogfile
 
-window = tkinter.Tk()
 
-master = Tk()
-Label(master, text='First Name').grid(row=0)
-Label(master, text='Last Name').grid(row=1)
-e1 = Entry(master)
-e2 = Entry(master)
-e1.grid(row=0, column=1)
-e2.grid(row=1, column=1)
+def perform():
+    source_path = source_in.get()
+    destination_path = destination_in.get()
+    ogfile.of(source_path, destination_path)
+    output.grid(row=3, column=1)
 
-window.mainloop()
+
+
+window = Tk()
+
+window.geometry("1280x720")
+window.title("File Organiser ~ Krish Suthar")
+
+source = Label(window, text='Enter address of folder which needs to be organised')
+destination = Label(window, text='Enter address of where to store organised folders?')
+output = Label(window, text="Files Organised Successfully!")
+
+source.config(font=("Courier", 16))
+destination.config(font=("Courier", 16))
+
+source_in = Entry(window, width=100)
+destination_in = Entry(window, width=100)
+
+btn = Button(window, text="Organise Now", width=20, height=2, bg='blue', fg='white')
+btn.config(font=("Courier", 16))
+btn.config(command=perform)                             # When I add parenthese after function name, it ~gives~ Throws errorS.
+
+source.grid(row=0, column=0)
+source_in.grid(row=0, column=1)
+destination.grid(row=1, column=0)
+destination_in.grid(row=1, column=1)
+btn.grid(row=2, column=1)
+
+
+mainloop()
